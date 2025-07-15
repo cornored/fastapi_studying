ec36c7141b46ab34c6736e7db45aefd81277ba34
Sasha <cornored@gmail.com>
2025-07-15 09:14:09 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from typing import Union
+import socket
+import datetime
+
+app = FastAPI()
+
+class ComputerSchema(BaseModel):
+    owner: str
+    description: Union[str, None] = Field(default=None, max_length=100)
+    OS: str
+    RAM: int = Field(ge=8, le=64)
+    CPU: str
+
+@app.get("/name")
+def name():
+    hostname = socket.gethostname()
+    return{"The hostname is:", hostname}
+
+@app.get("/time")
+def time():
+    now = datetime.datetime.now()
+    return{"The current time is:", now}
+
+existing_pc = []
+
+@app.post("/add_pc")
+def add_pc(pc: ComputerSchema):
+    existing_pc.append(pc)
+    return {"Success": True, "msg": "User added"}
+
+@app.get("/get_pc")
+def get_pc():
+    return existing_pc
